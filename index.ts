2eb91092fefe0177e27754ae7ec01f2ec8deec49
Azhar Ali Khan <azhar987k@gmail.com>
2023-08-26 13:11:38 +0500

    Initial and Final Commit

---
+++ index.ts
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+import inquirer from "inquirer"
+
+while(1){
+    let answers = await inquirer.prompt([
+        {
+            name: "num1",
+            type: "number",
+            message: "Enter First Number: "
+        },
+        {
+            name: "num2",
+            type: "numeber",
+            message: "Enter Second Number: "
+        },
+        {
+            name: "operation",
+            type: "input",
+            message: "Enter Operation, Valid Operations: +, -, *, / :    ",
+            validate: function(operation: string){
+                if(operation == '+' || operation == '-' || operation == '*' || operation == '/'){
+                    return true;
+                }
+                else{
+                    return `Invalid Operation. Valid Operations are +, -, *, /`;
+                }   
+            }
+        }
+    ])
+    
+    if(answers.operation == '+'){
+        
+        console.log("The Answer is: ", parseInt(answers.num1, 10) + parseInt(answers.num2, 10))
+    }
+    else if(answers.operation == '-'){
+        console.log("The Answer is: ", parseInt(answers.num1, 10) - parseInt(answers.num2, 10))
+    }
+    else if(answers.operation == 'x'){
+        console.log("The Answer is: ", parseInt(answers.num1, 10) * parseInt(answers.num2, 10))
+    }
+    else if(answers.operation == '/'){
+        console.log("The Answer is: ", parseInt(answers.num1, 10) / parseInt(answers.num2, 10))
+    }
+}
